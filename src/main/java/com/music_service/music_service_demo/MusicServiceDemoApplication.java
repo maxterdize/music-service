cb88f26e50fc86cf47b5e75c1f7a80c56c651b4c
maxterdize <maxter.rojas@gmail.com>
2024-07-21 21:11:53 -0600

    first commit
    foundation project

---
+++ src/main/java/com/music_service/music_service_demo/MusicServiceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.music_service.music_service_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicServiceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicServiceDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/music_service/music_service_demo/MusicServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.music_service.music_service_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicServiceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
